1e9e750d02e6d2570d100fd91803da503bb6b83b
bpmcdevitt <brendan@mcdevitt.tech>
2021-10-18 18:03:25 -0500

    initial commit

---
+++ lib/launchpadlib.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "launchpadlib/version"
+
+module Launchpadlib
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/launchpadlib/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Launchpadlib
+  VERSION = "0.1.0"
+end
